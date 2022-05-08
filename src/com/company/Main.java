79f7b151c68d0e5a4992278b6a3350ce2775fc94
Adham <adhams,ka7@gmail.com>
2022-05-08 16:31:57 +0200

    commit 2 adham

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    //commentt
+    //comment
     static Scanner scanner=new Scanner(System.in);
 
     public static void main(String[] args) {
