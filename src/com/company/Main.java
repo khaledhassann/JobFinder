45f32b7f187b410e355c9fe6ff65fb687f4894df
Adham <adhams,ka7@gmail.com>
2022-05-08 19:17:56 +0200

    main commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,71 @@ import java.util.Scanner;
 public class Main {
     //comment
     static Scanner scanner=new Scanner(System.in);
-
     public static void main(String[] args) {
-        login();
-
+        showOptions(login());
     }
-    static void login(){
-        System.out.println("Enter Username:");
-        String username=scanner.nextLine();
-        System.out.println("Enter Password:");
-        String password=scanner.nextLine();
-        Company company = new Company();
-        Admin Habiba=new Admin("Habiba","Habiba10","123",20,"habiba@gmail.com" , company);
-        Admin khaled=new Admin("khaled","Khaled10","321",20,"habiba@gmail.com" , company);
-        scanner.close();
+    static String login(){
+        while(true) {
+            System.out.println("Please specify the type of user:\n"+"1.Admin\n"+"2.Job Poster\n"+"3.Job Seeker\n");
+            String input = scanner.nextLine();
+            if(!(input.equals("1")||input.equals("2")||input.equals("3"))){
+                System.out.println("Invalid type of user");
+                System.out.println("-------------------------------------------------");
+                continue;
+            }
+            System.out.print("Enter Username:");
+            String username = scanner.nextLine();
+            System.out.print("Enter Password:");
+            String password = scanner.nextLine();
+            Company company = new Company();
+            Resume resume = new Resume("anjax");
 
+            Admin Habiba = new Admin("Habiba", "Habiba10", "123", 20, "habiba@gmail.com", company);
+            Job_Seeker Khaled = new Job_Seeker("khaled", "Khaled10", "1234", 20, "habiba@gmail.com", resume);
+            Job_Poster Omar = new Job_Poster("Omar", "Omar10", "12345", 20, "habiba@gmail.com", company);
 
-        if(khaled.validation(username,password)==true){
-            System.out.println("Logged in!");
-            return;
+            if(input.equals("1")){
+
+                if (Admin.validation(username, password)) {
+                    System.out.println("Logged in!");
+                    return input;
+                }
+                System.out.println("Invalid username or password , please try again.");
+                System.out.println("-------------------------------------------------");
+                continue;
+            }
+            if(input.equals("2")){
+                if (Job_Poster.validation(username, password)) {
+                    System.out.println("Logged in!");
+                    return input;
+                }
+                System.out.println("Invalid username or password , please try again.");
+                System.out.println("-------------------------------------------------");
+                continue;
+            }
+            if(input.equals("3")){
+                if (Job_Seeker.validation(username, password)) {
+                    System.out.println("Logged in!");
+                    return input;
+                }
+                System.out.println("Invalid username or password , please try again.");
+                System.out.println("-------------------------------------------------");
+                continue;
+            }
+
         }
-        System.out.println("Invalid");
+    }
+    static void showOptions(String n){
+
+        if(n.equals("1")){
+            Admin.options();
+        }
+        if(n.equals("2")){
+            Job_Poster.options();
+        }
+        if(n.equals("3")){
+            Job_Seeker.options();
+        }
+
     }
 }
